80f59d4b0d4853adcddb70cb22ba29b2866033d7
ANEES MEHMOOD <107695769+aneeshyder777@users.noreply.github.com>
2022-12-30 17:16:33 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,64 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  public employees: Employee[] = [];
+  
+
+  constructor(private employeeService: EmployeeService){}
+
+  ngOnInit(){
+    this.getEmployees();
+  }
+
+  public getEmployees(): void{
+    this.employeeService.getEmployees().subscribe(
+      (Response: Employee[]) => {
+          this.employees = Response;
+        
+      },
+      (error: HttpErrorResponse) => {
+          alert(error.message);
+      }
+
+    );
+
+  }
+
+
+  
+
+  
+
+  public onOpenModal(employee: Employee, mode: string): void {
+    const container = document.getElementById('main-container');
+    const button =document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if(mode === 'add'){
+      button.setAttribute('data-target', '#addEmployeeeModal');
+    }
+
+    if (mode === 'edit') {
+     
+      button.setAttribute('data-target', '#updateEmployeeModal');
+    }
+
+    if (mode === 'delete') {
+    
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+    container?.appendChild(button);
+    button.click();
+  }
+  
+}
